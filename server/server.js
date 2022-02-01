1cab0f97fe93bfb125fc45a806b1422d3afd3bce
Rafael Melloni Chacon Arnone <titico0306@gmail.com>
2022-02-01 12:36:00 -0300

    deploy

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const users = require('./routes/api/users')
-const articles = require('./routes/api/articles')
-const files = require('./routes/api/files')
-const { checkToken } = require('./middleware/auth');
+const users = require("./routes/api/users");
+const articles = require("./routes/api/articles");
+const files = require("./routes/api/files");
+const { checkToken } = require("./middleware/auth");
 
-const mongoUri = `mongodb://localhost/`
+const mongoUri = `${process.env.DB_HOST}`;
 mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
-app.use(bodyParser.json())
-app.use(checkToken)
-app.use("/api/users", users)
-app.use("/api/articles", articles)
-app.use("/api/files", files)
+app.use(bodyParser.json());
+app.use(checkToken);
+app.use("/api/users", users);
+app.use("/api/articles", articles);
+app.use("/api/files", files);
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log(`Server rinning on port ${port}`)
-})
+  console.log(`Server rinning on port ${port}`);
+});
